2fd7aa28bca5c71570962d418fb2b9b11d00acb7
ronazst <24581567+ronazst@users.noreply.github.com>
2023-07-04 23:33:05 +0800

    chore: add new script to deploy to aws

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # notion-ical-syncer
 Sync notion calendar to apple calendar
+
+### How to use it
+
+This project depends on AWS service, so you have to deploy it to AWS Lambda in order to use it.
+
+I already created a script to build and deploy this project, you just need to run following commands.
+
+```shell
+    git@github.com:ronazst/notion-ical-syncer.git
+    cd notion-ical-syncer
+    #such as: ./scripts/build-and-deploy-to-aws.sh ronazst us-east-1
+    ./scripts/build-and-deploy-to-aws.sh <customized-cloudformation-stack-suffix> <aws-region>
+```
+
+### Note
+
+* Deploy AWS resource may charge you monthly
+  * I already make the resource under free tier as possible as I can. But sadly, I can't avoid to use S3 inorder to simplify deployment process
+* Each deployment(with different suffix) will create 2 different AWS Cloudformation stack
+  * notion-ical-syncer-infra-<customized-cloudformation-stack-suffix>
+  * notion-ical-syncer-<customized-cloudformation-stack-suffix>
+* Once you want to delete it, go to AWS Cloudformation to delete related stack manually
+* The S3 bucket will not be deleted after you deleted AWS Cloudformation, so you have to delete related S3 manually
---
+++ scripts/build-and-deploy-to-aws.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e -u -o pipefail
+
+STACK_SUFFIX="$1"
+AWS_REGION="$2"
+
+if [[ $# != 2 ]] || [[ $1 =  "" ]] || [[ $1 =  "-h" ]] || [[ $1 =  "--help" ]] || [[ $2 =  "" ]]; then
+  echo "Script to run things locally"
+  echo "Usage:"
+  echo "  $0 stack_suffix aws_region"
+  exit 1
+fi
+
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o output/syncer cmd/syncer.go && cd output && zip syncer.zip syncer && cd ..
+
+PROJECT_DIRECTORY="$(dirname "$(cd "$(dirname "$0")" && pwd)")"
+
+aws cloudformation deploy \
+    --region "$AWS_REGION" \
+    --template-file "$PROJECT_DIRECTORY"/scripts/aws/infra.yml \
+    --stack-name notion-ical-syncer-infra-"$STACK_SUFFIX"
+
+UPLOAD_BUCKET_NAME="$(aws cloudformation describe-stacks \
+    --region "$AWS_REGION" \
+    --stack-name notion-ical-syncer-infra-"$STACK_SUFFIX" \
+    --query "Stacks[0].Outputs[0].OutputValue" | tr -d  '"')"
+
+aws cloudformation package \
+    --region "$AWS_REGION" \
+    --template-file "$PROJECT_DIRECTORY"/scripts/aws/template.yml \
+    --s3-bucket "$UPLOAD_BUCKET_NAME" \
+    --output-template-file "$PROJECT_DIRECTORY"/output/output-template.yml
+
+aws cloudformation deploy \
+    --region "$AWS_REGION" \
+    --template-file "$PROJECT_DIRECTORY"/output/output-template.yml \
+    --stack-name notion-ical-syncer-"$STACK_SUFFIX" \
+    --capabilities CAPABILITY_NAMED_IAM
